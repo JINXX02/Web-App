a74e2751b7b6927c7bd05aa4b83189fa8f91200a
JINXX02 <145373315+JINXX02@users.noreply.github.com>
2025-08-13 12:50:40 -0700

    added image

--- script.js
+++ script.js
@@ -30,12 +30,12 @@ async function getAllRecords() {
 
         newHtml += `
         <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">Some quick example text.</p>
-                <a href="${site}" class="card-link">Bakery Link</a>
-                <a class="mt-1 btn-primary mt-2" href="index.html?id=${data.records[i].id}">View Details</a>
-            </div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
         </div>
         
         `;
@@ -43,34 +43,34 @@ async function getAllRecords() {
 
       getResultElement.innerHTML = newHtml;
     });
-  }
+}
 
-  async function getOneRecord(id) {
-    let getResultElement = document.getElementById("bakeries");
+async function getOneRecord(id) {
+  let getResultElement = document.getElementById("bakeries");
 
-    const options = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer patyyUVgV0jDN68AP.c4716268bb89fcf20813c88f10403594c2e1e6c4be1cb9fd7bca307d838ef39a`,
-      },
-    };
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patyyUVgV0jDN68AP.c4716268bb89fcf20813c88f10403594c2e1e6c4be1cb9fd7bca307d838ef39a`,
+    },
+  };
 
-    await fetch(`https://api.airtable.com/v0/appEvTziueCGBcypI/Bakeries/${id}`, options)
+  await fetch(`https://api.airtable.com/v0/appEvTziueCGBcypI/Bakeries/${id}`, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      getResultElement.innerHTML = ""; 
+      getResultElement.innerHTML = "";
 
       let newHtml = "";
 
-        let name = data.records.fields["Name"];
-        let address = data.records.fields["Address"];
-        let phone = data.records.fields["Phone"];
-        let hours = data.records.fields["Hours"];
-        let site = data.records.fields["Site"];
-        let rating = data.records.fields["Google Rating"];
+      let name = data.records.fields["Name"];
+      let address = data.records.fields["Address"];
+      let phone = data.records.fields["Phone"];
+      let hours = data.records.fields["Hours"];
+      let site = data.records.fields["Site"];
+      let rating = data.records.fields["Google Rating"];
 
-        newHtml += `
+      newHtml += `
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${name}</h5>
@@ -81,16 +81,16 @@ async function getAllRecords() {
         </div>
         
         `;
-      
+
 
       getResultElement.innerHTML = newHtml;
 
-  });
+    });
 }
 
 let idParams = window.location.search.split("?id=");
 if (idParams.length >= 2) {
-    getOneRecord(idParams[1]);
-  } else {
-      getAllRecords()
-    }
+  getOneRecord(idParams[1]);
+} else {
+  getAllRecords()
+}
